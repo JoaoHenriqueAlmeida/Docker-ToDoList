47d683f6f75736810881566d8a571ff990f68c7b
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-01-19 17:59:04 -0300

    Remove conteúdo com direitos autorais

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rootDir: './__tests__/integration',
-  testSequencer: '../sequencer.js',
-  setupFilesAfterEnv: ['../setup.js'],
-  testRegex: './*\\.test\\.js$',
-  testTimeout: 300000,
-};
